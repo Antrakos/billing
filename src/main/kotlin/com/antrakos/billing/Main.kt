36075b7e2a80b7a14ce9954f1fba7000484fc56b
antrakos <taraszubrei@gmail.com>
2017-11-30 19:50:27 +0200

    Custom ObjectMapper to configure serialization

--- src/main/kotlin/com/antrakos/billing/Main.kt
+++ src/main/kotlin/com/antrakos/billing/Main.kt
@@ -1,7 +1,11 @@
 package com.antrakos.billing
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
 
 /**
@@ -10,7 +14,13 @@ import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-open class Application
+open class Application {
+    @Bean
+    open fun objectMapper(): ObjectMapper = ObjectMapper().
+            findAndRegisterModules()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
