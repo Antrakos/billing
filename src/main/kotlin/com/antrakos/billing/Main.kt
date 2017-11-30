fe899ae6962ab7fd0c97ec3760388a1569d30a43
antrakos <taraszubrei@gmail.com>
2017-11-30 20:56:12 +0200

    Swagger 2 Docs

--- src/main/kotlin/com/antrakos/billing/Main.kt
+++ src/main/kotlin/com/antrakos/billing/Main.kt
@@ -1,5 +1,6 @@
 package com.antrakos.billing
 
+import com.antrakos.billing.web.SecureUser
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.*
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 /**
  * @author Taras Zubrei
@@ -14,12 +22,35 @@ import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
 open class Application {
     @Bean
     open fun objectMapper(): ObjectMapper = ObjectMapper().
             findAndRegisterModules()
             .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
             .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+
+    @Bean
+    open fun swaggerApi(): Docket = Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.antrakos.billing"))
+            .paths(PathSelectors.any())
+            .build()
+            .securitySchemes(listOf(BasicAuth("Authorization")))
+            .securityContexts(listOf(SecurityContext.builder().securityReferences(listOf(SecurityReference("Authorization", arrayOf(AuthorizationScope("global", "accessEverything"))))).build()))
+            .apiInfo(apiInfo)
+            .ignoredParameterTypes(SecureUser::class.java)
+
+    private val apiInfo = ApiInfo(
+            "Billing Service",
+            "Billing system documentation",
+            "1.0.0",
+            "Terms of Service",
+            Contact("John Doe", "www.example.com", "myeaddress@company.com"),
+            "Apache License 2.0",
+            "http://www.apache.org/licenses/LICENSE-2.0",
+            emptyList()
+    )
 }
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/antrakos/billing/web/security.kt
+++ src/main/kotlin/com/antrakos/billing/web/security.kt
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -40,6 +41,10 @@ open class SecurityConfig : WebSecurityConfigurerAdapter() {
                     response.status = HttpServletResponse.SC_BAD_REQUEST
                     response.writer.println("HTTP Status 400 - " + authException.message)
                 }
+    }
+
+    override fun configure(web: WebSecurity) {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**")
     }
 
     @Bean
